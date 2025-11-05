6b91ba4afeb04b6572c6ea068763aab82dcf0268
Ayeshas09 <asurryya@gmail.com>
2025-11-05 10:24:33 +0500

    feat: add hmac signature verifier

---
+++ src/SignatureVerifier/SignatureVerifierInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\SignatureVerifier;
+
+use Symfony\Component\HttpFoundation\Request;
+
+interface SignatureVerifierInterface
+{
+  /**
+   * Verifies the signature of a request.
+   *
+   * @param Request $request
+   * @return VerificationFailure | true
+   */
+  public function verify(Request $request);
+}
---
+++ src/Types/Sdk/VerificationFailure.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Types\Sdk;
+
+class VerificationFailure
+{
+  private string $errorMessage;
+
+  public function __construct(string $errorMessage)
+  {
+    $this->errorMessage = $errorMessage;
+  }
+
+  public static function init(string $errorMessage): VerificationFailure
+  {
+    return new VerificationFailure($errorMessage);
+  }
+
+  public function getErrorMessage(): string
+  {
+    return $this->errorMessage;
+  }
+}
